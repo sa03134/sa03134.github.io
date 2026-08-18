c72a170145503e3e76302db3f59cd776373f556a
Jung Woo Sohn <70755880+sa03134@users.noreply.github.com>
2026-08-19 02:38:47 +0900

    Refresh PWA cache for seal-brand redesign

--- ilshinhoe/sw.js
+++ ilshinhoe/sw.js
@@ -1,5 +1,5 @@
-const CACHE='ilshinhoe-shell-v9';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+const CACHE='ilshinhoe-shell-v10';
+const SHELL=['./','./index.html','./manifest.webmanifest','./brand-logo.svg','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request)))});
